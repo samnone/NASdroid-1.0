89ff63bc1037e216145ccb352a63bcc39ce57f19
samnone <paul.cornejo.lira@gmail.com>
2026-03-20 10:26:45 -0300

    Initial commit

--- src/config.ts
+++
@@ -1,3 +0,0 @@
-export const API_BASE_URL = window.location.origin.includes('localhost') || window.location.origin.includes('run.app')
-  ? '' // Use relative paths in dev/web
-  : 'http://localhost:8080'; // Default port for the native Android server (Ktor)
